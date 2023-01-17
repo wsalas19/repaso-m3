b0f08b1c3fac689316d64bafa57955197aa56726
William Salas <willbullet52@gmail.com>
2023-01-17 18:47:29 -0500

    initial commit

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
-'use strict';
+"use strict";
 
-var express = require('express');
-var app = express();
-var characters = require('./routes/index.js')
-var model = require('./models/model.js')
-module.exports = app; // Esto es solo para testear mas facil
+const express = require("express");
+const app = express();
+const router = require("./routes");
+const PORT = 3000;
 
 // Acuerdense de agregar su router o cualquier middleware que necesiten aca
 
-
+app.use(express.json());
+app.use(router);
 
 // El condicional es solo para evitar algun problema de tipo EADDRINUSE con mocha watch + supertest + npm test.
-if (!module.parent) app.listen(3000);
+if (!module.parent)
+	app.listen(PORT, () => {
+		console.log(`%s listening at ${PORT}`);
+	});
+
+module.exports = app; // Esto es solo para testear mas facil
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,38 @@
-'use strict';
+"use strict";
 
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 module.exports = router;
 
+const models = require("../models/model");
+
 // escriban sus rutas acá
 // siéntanse libres de dividir entre archivos si lo necesitan
+router.get("/families", (req, res) => {
+	res.status(200).send(models.listFamilies());
+});
+router.post("/families", (req, res) => {
+	res.status(200).json(models.addFamily(req.body.family));
+});
+
+router.get("/characters", (req, res) => {
+	res.status(200).json(models.listCharacter());
+});
+router.post("/characters", (req, res) => {
+	try {
+		const { name, age, family } = req.body;
+		res.status(200).json(models.addCharacter(name, age, family));
+	} catch (error) {
+		res.status(400).send({ msg: error.message });
+	}
+});
+
+router.get("/characters/:name", (req, res) => {
+	const { name } = req.params;
+	const { pluck } = req.query;
+	if (pluck) {
+		res.status(200).json(models.listCharacter(name, pluck));
+	} else {
+		res.status(200).json(models.listCharacter(name));
+	}
+});
