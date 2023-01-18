9d3c326bbfe01572e8ca2c7c34bef87f55dd609c
William Salas <willbullet52@gmail.com>
2023-01-17 20:30:43 -0500

    repaso iniciado

--- models/model.js
+++ models/model.js
@@ -17,66 +17,26 @@ module.exports = {
 	addFamily: function (name) {
 		// Agrega el apellido de una nueva familia verificando que no exista
 		// Debe retornar el nombre de la familia agregado o existente
-		if (families.some((s) => s === name)) return name;
-		else {
-			families.push(name);
-			return name;
-		}
 	},
 	listFamilies: function () {
 		// Devuelve un arreglo con todas las familias
-		return families;
 	},
 	listCharacter: function (family, pluckName) {
 		// Devuelve un arreglo con todos los personajes
 		// Si recibe un nombre de familia como parámetro debería filtrar solo los personajes de ella
 		// Si recibe un segundo parámetro en true debe devolver únicamente los nombres de los personajes
-
-		if (pluckName && family) {
-			return characters
-				.filter((c) => families[c.familyId - 1] === family)
-				.map((c) => c.name);
-		}
-
-		if (family) {
-			return characters.filter((c) => {
-				console.log(families[c.familyId - 1]);
-				return families[c.familyId - 1] === family;
-			});
-		}
-		return characters;
 	},
 	addCharacter: function (name, age, family) {
 		// Agrega un nuevo personaje, inicialmente sus frases (quotes) deben estar "vacias"
 		// Adicionalmente va a ser necesario guardar el número de familia y no su nombre
 		// El número de familia debe empezar desde 1 y no desde 0.
 		// Debe retornar el personaje creado
-		let check = families.some((f) => f === family);
-		if (check) {
-			let familyIndex = families.findIndex((f) => f === family) + 1;
-			let char = { name, age, familyId: familyIndex, quotes: [] };
-			characters.push(char);
-			return char;
-		}
-		throw new Error("La familia ingresada no existe");
 	},
 	addQuote: function (name, quote) {
 		// Agrega una nueva frase a un personaje en particular con el formato:
 		// {text: "Este es el texto de la frase", season: 3}
-		let char = characters.find((c) => c.name === name);
-		if (char) {
-			if (quote.text) {
-				let newQuote = { text: quote.text, season: quote.season || false };
-				char.quotes.push(newQuote);
-				return { msg: "Frase agregada correctamente" };
-			}
-			return "no se ha encotrado texto que agregar";
-		}
 	},
 	showQuotes: function (name) {
 		// Devuelve todas las frases de un personaje en particular
-		let char = characters.find((c) => c.name === name);
-		if (!char) return [];
-		return char.quotes;
 	},
 };
--- routes/index.js
+++ routes/index.js
@@ -9,38 +9,5 @@ const models = require("../models/model");
 // escriban sus rutas acá
 // siéntanse libres de dividir entre archivos si lo necesitan
 router.get("/families", (req, res) => {
-	res.status(200).send(models.listFamilies());
-});
-router.post("/families", (req, res) => {
-	res.status(200).json(models.addFamily(req.body.family));
-});
-
-router.get("/characters", (req, res) => {
-	res.status(200).json(models.listCharacter());
-});
-router.post("/characters", (req, res) => {
-	try {
-		const { name, age, family } = req.body;
-		res.status(200).json(models.addCharacter(name, age, family));
-	} catch (error) {
-		res.status(400).send({ msg: error.message });
-	}
-});
-
-router.get("/characters/:name", (req, res) => {
-	const { name } = req.params;
-	const { pluck } = req.query;
-	if (pluck) {
-		res.status(200).json(models.listCharacter(name, pluck));
-	} else {
-		res.status(200).json(models.listCharacter(name));
-	}
-});
-
-router.get("/quotes", (req, res) => {
-	res.status(200).send(models.showQuotes(req.body.name));
-});
-router.post("/quotes", (req, res) => {
-	const { name, quote } = req.body;
-	res.status(200).json(models.addQuote(name, quote));
+	//......tu codigo aqui
 });
